fea8e5b5c79fa067ff375c313cba57739656f12e
albertoaguadosegovia <alberto.aguadosegovia@cieep.com>
2023-10-05 10:04:10 +0200

    Initial Commit

---
+++ ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio1
+//
+//  Created by Alberto  Aguado Segovia on 05/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
